342a420cd15f2ca645c5a6598229d9cfbb4599f7
YSH <YSH@DESKTOP-D77UQ50>
2020-12-24 23:52:35 +0900

    '201224'

---
+++ Exercise/src/Dectwentyfour/NewExample.java
@@ -0,0 +1,21 @@
+package Dectwentyfour;
+
+public class NewExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int var1 = 100;
+		System.out.println(var1);
+		
+		int var2 = 0101;		// 8진수
+		System.out.println(var2);
+		
+		int var3 = 0x100;		// 16진수		0 1 2 3 4 5 6 7 8 9 A B C D E F 10 
+		System.out.println(var3);
+		
+		int var4 = 0b1011101;		// 2진수
+		System.out.println(var4);
+	}
+
+}
